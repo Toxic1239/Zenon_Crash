7f56c478fd5e4d4704d0aad2deb934d960b01477
TOXXIC MD <161505113+Toxic1239@users.noreply.github.com>
2024-07-21 16:04:15 +0100

    Draken Md 👾

--- settings.js
+++ settings.js
@@ -42,7 +42,7 @@ global.autoswview = true //auto view status/story
 //v2 is link + image menu,
 //v3 is video menu,
 //v4 is call end menu
-global.typemenu = 'v1'
+global.typemenu = 'v2'
 
 //text bug
 global.xbugtex = {
